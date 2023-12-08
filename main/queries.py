afea2f6dff9f92777f54dc5788d627cad605f05a
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2023-12-08 12:35:24 +0700

    fix duplicate results caused by team names

--- main/queries.py
+++ main/queries.py
@@ -3,10 +3,15 @@ from .models import graph
 def search_all_in_matches():
     result = graph.query(
         """
-        select distinct ?iri (substr(str(?iri), 22) as ?label) where {
-        ?iri :team ?team.
-        } 
-        order by ?label
+        select distinct ?iri (substr(str(?iri), 22) as ?label) ?team1_label ?team2_label ?date_literal where {
+        ?iri :team ?team1.
+        ?team1 rdfs:label ?team1_label.
+        ?iri :team ?team2.
+        ?team2 rdfs:label ?team2_label.
+        filter(?team1_label < ?team2_label)
+        ?iri :date ?date_literal.
+        }
+        order by ?date_literal
         """
     )
     return result
@@ -47,7 +52,7 @@ def search_identifier(bindings):
 def search_detailed_matches(bindings):
     result = graph.query(
         """
-        select distinct ?iri (substr(str(?iri), 22) as ?label) ?date_literal ?team1_label ?team2_label ?winner_label where {
+        select distinct ?iri (substr(str(?iri), 22) as ?label) ?team1_label ?team2_label ?date_literal where {
         ?iri :city ?city.
         ?city rdfs:label ?city_label.
         ?iri :date ?date_literal.
@@ -60,6 +65,7 @@ def search_detailed_matches(bindings):
         ?team1 rdfs:label ?team1_label.
         ?iri :team ?team2.
         ?team2 rdfs:label ?team2_label.
+        filter(?team1_label < ?team2_label)
         ?iri :toss_decision ?toss_decision_literal.
         ?iri :toss_winner ?toss_winner.
         ?toss_winner rdfs:label ?toss_winner_label.
@@ -73,7 +79,7 @@ def search_detailed_matches(bindings):
         ?iri :win_by_type ?win_by_type_literal.
         ?iri :winner ?winner.
         ?winner rdfs:label ?winner_label.
-        } order by ?label
+        } order by ?date_literal
         """
     , initBindings=bindings)
     return result
